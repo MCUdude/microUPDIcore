d089c2eea70ff2852fde7ad43b9ebe4cbfc727e2
per1234 <accounts@perglass.com>
2019-08-22 00:25:58 -0700

    Remove unnecessary files from branch
    These files were added to the branch due to it being based on master, but we only need the Boards Manager files in this branch so any other files will only lead to confusion.

--- avr/scripts/dummy-preproc-macros-command.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-REM A dummy executable to give the compiler recipes in platform.txt something to run. It must generate a dummy preprocessing file in order for compilation to succeed.
-echo > "%1/%2"
